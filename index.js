2092e903d9dae8519faf10e9ab184505734f7439
vignesh <vigneshfornavy@gmail.com>
2023-11-17 18:04:20 +0530

    structured

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
 const express = require("express");
 const app = express();
 const port = 8080;
+const path = require("path");
+app.set("view engine","ejs");
+app.set("views",path.join(__dirname,"/views"));
+app.use(express.static(path.join(__dirname,"public/css")));
+app.use(express.static(path.join(__dirname,"public/js")));
+app.use(express.urlencoded({extended:true}));
+
+
+
 
 
 app.listen(port,()=>{
     console.log(`The port is running on ${port}`);
+})
+
+app.get("/",(req,res)=>{
+    res.render("home.ejs");
 })
